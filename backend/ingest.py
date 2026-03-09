e190b969e1a1b48befb39d8c1aede31ed5cce048
Vinzy <vinzpedres@gmail.com>
2026-03-09 23:06:14 +0800

    refactor: changed import for newer langchain model

--- backend/ingest.py
+++ backend/ingest.py
@@ -5,7 +5,7 @@
 # ============================================================
 
 from pypdf import PdfReader
-from langchain.schema import Document
+from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
